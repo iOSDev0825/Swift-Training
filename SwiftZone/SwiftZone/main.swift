7b4ec65e857ffec85214a2b0db183cbaec3e3fbd
Gaurav Gupta <1276964+ggthedev@users.noreply.github.com>
2022-11-01 00:51:53 +0530

    strings and characters example

--- SwiftZone/SwiftZone/main.swift
+++ SwiftZone/SwiftZone/main.swift
@@ -4,68 +4,32 @@
 //
 //
 
-var name = "Gaurav" //stored variable or stored property
+var name = "Gaurav"
+var firstChar:Character = .init("C") //basic character declaration
 
-    //following is an example of read-only variable/property
-var upperCaseName:String {
-    name.uppercased()
-}
-
-var _name:String = ""
-    //Following is an example of property having getters and setters
-var computedName:String{
-    get {
-        _name.lowercased()
-    }
-    set{
-        _name = newValue.uppercased()
-    }
-}
-
-//Following shows the concept of Observers
-var doesSunRisesInEast = true {
-    didSet{
-        print(oldValue)
-    }
-    willSet{
-        if newValue == true{
-            print("Correctly set")
-        }
-        else {
-            print("Screwed up concepts on sunset and sunrise as of now")
-        }
-    }
-}
-
-print(doesSunRisesInEast)
-doesSunRisesInEast = false
-
-
-//Introduction to Tuples
-let httpResponse = (400, "Failure due to missing element")
-print(httpResponse.0)
-print(httpResponse.1)
-
-let setOfG8 = Set<String>(["USA","RUSSIA","CHINA","JAPAN","GERMANY","SOUTH KOREA","ITALY","FRANCE"])
-for element in setOfG8.enumerated(){
-    print(element.offset)
-    print(element.element)
-}
-
-
-//Optionals
-var dictOfCountriesAndCapitals = ["India" :  "Delhi"]
-dictOfCountriesAndCapitals["USA"] = "Washington D.C"
-print(dictOfCountriesAndCapitals["Afghanistan"])
-print(type(of: dictOfCountriesAndCapitals["India"]))
-print(type(of: setOfG8.first))
-var newArray:[Int] = .init()
-//print(newArray[0])
-print(newArray.first)
-var name_first:Optional<String> = .some("Gaurav")
-var name_second:String!  = "GG"
-var name_captured:String = name_second
-print(type(of: name_captured))
-name_captured = name_first != nil ? name_first!:"NA"
-name_second = name_second != nil ? name_second:"NA"
-
+var firstCharOfName = name.first
+print(firstCharOfName)
+print(type(of: name[name.startIndex]))
+//name = ""
+//print(type(of: name[name.startIndex])) //using [] notation, collection can not be empty, else will cause error
+//Fatal error: String index is out of bounds
+var droppedChar = name.dropFirst()
+print(type(of: droppedChar))
+print(droppedChar)
+print(name)
+var removeChar = name.removeFirst()
+print(removeChar)
+print(type(of: removeChar))
+print(name)
+//print(name[name.endIndex]) //Fatal error: String index is out of bounds
+var effectiveLastIndex = name.index(before: name.endIndex)
+print(name[effectiveLastIndex])
+effectiveLastIndex = name.index(name.endIndex, offsetBy: -1)
+print(name[effectiveLastIndex])
+print(name)
+var poppedChar = name.popLast()
+print(type(of: poppedChar))
+print(poppedChar)
+print(name)
+print(name.last)
+print(name)
