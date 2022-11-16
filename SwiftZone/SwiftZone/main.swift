518c560230aeb20a9a622a7b14b9b4528d890539
Gaurav Gupta <1276964+ggthedev@users.noreply.github.com>
2022-11-16 20:54:35 +0530

    introductin to functions basic signature

--- SwiftZone/SwiftZone/main.swift
+++ SwiftZone/SwiftZone/main.swift
@@ -4,32 +4,22 @@
 //
 //
 
-var name = "Gaurav"
-var firstChar:Character = .init("C") //basic character declaration
+func appendHello(toString str:String) -> String {
+    "Hello "+str
+}
 
-var firstCharOfName = name.first
-print(firstCharOfName)
-print(type(of: name[name.startIndex]))
-//name = ""
-//print(type(of: name[name.startIndex])) //using [] notation, collection can not be empty, else will cause error
-//Fatal error: String index is out of bounds
-var droppedChar = name.dropFirst()
-print(type(of: droppedChar))
-print(droppedChar)
-print(name)
-var removeChar = name.removeFirst()
-print(removeChar)
-print(type(of: removeChar))
-print(name)
-//print(name[name.endIndex]) //Fatal error: String index is out of bounds
-var effectiveLastIndex = name.index(before: name.endIndex)
-print(name[effectiveLastIndex])
-effectiveLastIndex = name.index(name.endIndex, offsetBy: -1)
-print(name[effectiveLastIndex])
-print(name)
-var poppedChar = name.popLast()
-print(type(of: poppedChar))
-print(poppedChar)
-print(name)
-print(name.last)
-print(name)
+print(appendHello(toString: "Radhika"))
+
+func appendHelloLala(str:String) -> String {
+    "Hello  LALA "+str
+}
+
+print(appendHelloLala(str: "Radhika"))
+
+//"_" here is ignore
+func appendRocks(_ str:String) -> String {
+   "\(str) Rocks"
+}
+
+
+print(appendRocks("Radhika"))
