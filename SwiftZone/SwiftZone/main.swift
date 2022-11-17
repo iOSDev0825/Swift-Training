4641e327408fe3e9779c34bdc021f63c2e77cf80
Gaurav Gupta <1276964+ggthedev@users.noreply.github.com>
2022-11-17 23:37:40 +0530

    introduction to functions

--- SwiftZone/SwiftZone/main.swift
+++ SwiftZone/SwiftZone/main.swift
@@ -23,3 +23,34 @@ func appendRocks(_ str:String) -> String {
 
 
 print(appendRocks("Radhika"))
+
+var appendSomething:((String) -> String) = { str in
+    "Hello "+str
+}
+print(type(of: appendSomething))
+print(appendSomething("Radhikaa"))
+appendSomething = appendHelloLala(str:)
+print(type(of: appendSomething))
+print(appendSomething("Radhikaa"))
+appendSomething = appendRocks(_:)
+print(type(of: appendSomething))
+print(appendSomething("Radhikaa"))
+
+var name = appendHelloLala(str: "Radhika")
+print(type(of: name))
+var newClosure = appendSomething
+print(newClosure("SSS"))
+let array = [1,2,3,4,5,6,7,8]
+let evenArray = array.filter { num in
+    print(num)
+    return num % 2 == 0
+}
+print(evenArray)
+
+func multipleOf3(num:Int) -> Bool{
+    num % 3 == 0
+}
+
+let arrayOfMultipleOf3 = array.filter(multipleOf3(num:))
+print(arrayOfMultipleOf3)
+
